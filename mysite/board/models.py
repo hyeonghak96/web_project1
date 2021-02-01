928d29678538bda7a10fb849c96417d4f8ec94c0
rewcho <rewcho@gmail.com>
2021-02-01 13:50:20 +0900

    rewcho try

--- mysite/board/models.py
+++ mysite/board/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from tinymce.models import HTMLField
 # Create your models here.
+
+class Board(models.Model):
+    title       = models.CharField(verbose_name='TITLE', max_length=50)
+    description = models.CharField('DESCRIPTION', max_length=100,
+                            blank=True, help_text='simple description text.')
+    content     = HTMLField('CONTENTC')                                                    #  models.TextField('CONTENT')
+    create_dt   = models.DateTimeField('CREATE DATE', auto_now_add=True)
+    modify_dt   = models.DateTimeField('MODIFY DATE', auto_now=True)
+    tags = TaggableManager(blank=True)
+
+    hit         = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    'tinymce',
+    'taggit.apps.TaggitAppConfig',
+    'taggit_templatetags2',
 ]
 
 MIDDLEWARE = [
