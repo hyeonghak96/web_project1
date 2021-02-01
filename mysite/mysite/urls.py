47fae87b3facd76f0a9225c74abc6e9fdf880934
Ryu <rjh7693@gmail.com>
2021-02-01 16:36:31 +0900

    1

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from bookmark.views import BookmarkLV, BookmarkDV
+
+
 from django.urls import path, include
 
-from mysite.views import HomeView, UserCreateView, UserCreateDoneTV
+from mysite.views import HomeView
 
 
 urlpatterns = [
