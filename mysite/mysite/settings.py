7d792c0baf65d3485f83e19973c1114c4eb31d46
Hyeonghak <qorgudgkr@naver.com>
2021-02-01 17:25:14 +0900

    third commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'assessment.apps.AssessmentConfig', #추가
+    
 ]
 
 MIDDLEWARE = [
